a0942d9365b8498acd57eec2eb0c9ee8bc1d7af0
christinasamson <156719832+christinasamson@users.noreply.github.com>
2024-05-30 20:24:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//This line selects the element with the class .nav-toggle from the document and assigns it to the constant variable//
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
